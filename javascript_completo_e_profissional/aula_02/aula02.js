ae03e0b4d71291f80c9f0188a07874b0e4219cb5
Lucas <lucas.pereira@animati.com.br>
2026-03-16 10:37:58 -0300

    Aula 02: modo strict e variáveis.

---
+++ javascript_completo_e_profissional/aula_02/aula02.js
@@ -0,0 +1,9 @@
+"use strict" //não permite que variáveis sem definições funcionem
+
+// armazenamento de valor
+let nome = "Lucas";
+let cidade = "Rio de Janeiro";
+
+console.log(nome);
+console.log("Nome = " + nome);
+console.log(cidade);
